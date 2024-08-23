06b8ff7b33cfadd064e74efd803cb8bee05328c7
Eric Passmore <eric.passmore@gmail.com>
2024-08-23 10:30:50 -0700

    better action dir

--- generate_action_json.sh
+++ generate_action_json.sh
@@ -1,7 +1,10 @@
 #!/bin/env bash
 
 NETWORK=${1:-LOCAL}
-CONTRACT_DIR="/local/eosnetworkfoundation/repos/eos-system-contracts/build/contracts"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ACTIONS_DIR="${SCRIPT_DIR}/actions"
+[ ! -d $ACTIONS_DIR ] && mkdir -p $ACTIONS_DIR
+EOS_CONTRACT_DIR=/local/eosnetworkfoundation/repos/eos-system-contracts/build/contracts
 
 ENDPOINT=http://127.0.0.1:8888
 JUNGLE=https://jungle4.cryptolions.io:443
@@ -22,66 +25,66 @@ fi
 ## CREATE JSON TRANSACTIONS FOR FEATURE ACTIVATIONS
 # DISABLE_DEFERRED_TRXS_STAGE_1
 cleos --url $ENDPOINT push action eosio activate '["fce57d2331667353a0eac6b4209b67b843a7262a848af0a49a6e2fa9f6584eb4"]' -s -d \
-    --json-file ${CONTRACT_DIR}/activate-disable-deferred-1-action.json --expiration 8640000
+    --json-file ${ACTIONS_DIR}/activate-disable-deferred-1-action.json --expiration 8640000
 # DISABLE_DEFERRED_TRXS_STAGE_2
 cleos --url $ENDPOINT push action eosio activate '["09e86cb0accf8d81c9e85d34bea4b925ae936626d00c984e4691186891f5bc16"]' -s -d \
-    --json-file ${CONTRACT_DIR}/activate-disable-deferred-2-action.json --expiration 8640000
+    --json-file ${ACTIONS_DIR}/activate-disable-deferred-2-action.json --expiration 8640000
 # BLS_PRIMITIVES2
 cleos --url $ENDPOINT push action eosio activate '["63320dd4a58212e4d32d1f58926b73ca33a247326c2a5e9fd39268d2384e011a"]' -s -d \
-    --json-file ${CONTRACT_DIR}/activate-bls-primitives-2-action.json --expiration 8640000
+    --json-file ${ACTIONS_DIR}/activate-bls-primitives-2-action.json --expiration 8640000
 # SAVANNA
 cleos --url $ENDPOINT push action eosio activate '["cbe0fafc8fcc6cc998395e9b6de6ebd94644467b1b4a97ec126005df07013c52"]' -s -d \
-    --json-file ${CONTRACT_DIR}/activate-savanna.json --expiration 8640000
+    --json-file ${ACTIONS_DIR}/activate-savanna.json --expiration 8640000
 # SET SYSTEM CONTRACT CODE AND ABI
-cleos --url $ENDPOINT set contract eosio ${CONTRACT_DIR}/eosio.system eosio.system.wasm eosio.system.abi -s -d \
-    --json-file ${CONTRACT_DIR}/setcontract-eosio.system.json --expiration 8640000
+cleos --url $ENDPOINT set contract eosio ${EOS_CONTRACT_DIR}/eosio.system eosio.system.wasm eosio.system.abi -s -d \
+    --json-file ${ACTIONS_DIR}/setcontract-eosio.system.json --expiration 8640000
 
 # CREATE EMPTY SHELL TRANSACTION THAT WILL HOLD OUR ACTIONS
-cat ${CONTRACT_DIR}/activate-disable-deferred-1-action.json | jq 'del(.actions)' | head -8 > ${CONTRACT_DIR}/start-shell-transaction.json
-cat ${CONTRACT_DIR}/activate-disable-deferred-1-action.json | jq 'del(.actions)' | tail -4 > ${CONTRACT_DIR}/end-shell-transaction.json
+cat ${ACTIONS_DIR}/activate-disable-deferred-1-action.json | jq 'del(.actions)' | head -8 > ${ACTIONS_DIR}/start-shell-transaction.json
+cat ${ACTIONS_DIR}/activate-disable-deferred-1-action.json | jq 'del(.actions)' | tail -4 > ${ACTIONS_DIR}/end-shell-transaction.json
 
 # using our shell create new transaction with many inline actions
-cp  ${CONTRACT_DIR}/start-shell-transaction.json ${CONTRACT_DIR}/PREPARE_SAVANNA_ACTIONS.json
+cp  ${ACTIONS_DIR}/start-shell-transaction.json ${ACTIONS_DIR}/PREPARE_SAVANNA_ACTIONS.json
 # open actions array
-printf '"actions": [' >> ${CONTRACT_DIR}/PREPARE_SAVANNA_ACTIONS.json
+printf '"actions": [' >> ${ACTIONS_DIR}/PREPARE_SAVANNA_ACTIONS.json
 # append actions with comma sep
 for file in activate-disable-deferred-1-action.json activate-disable-deferred-2-action.json activate-bls-primitives-2-action.json activate-savanna.json
 do
-  cat ${CONTRACT_DIR}/${file} | jq '.actions[]' >> ${CONTRACT_DIR}/PREPARE_SAVANNA_ACTIONS.json
-  printf "," >> ${CONTRACT_DIR}/PREPARE_SAVANNA_ACTIONS.json
-  rm ${CONTRACT_DIR}/${file}
+  cat ${ACTIONS_DIR}/${file} | jq '.actions[]' >> ${ACTIONS_DIR}/PREPARE_SAVANNA_ACTIONS.json
+  printf "," >> ${ACTIONS_DIR}/PREPARE_SAVANNA_ACTIONS.json
+  rm ${ACTIONS_DIR}/${file}
 done
 # add system contract set code action
-cat ${CONTRACT_DIR}/setcontract-eosio.system.json | jq '.actions[0]' >> ${CONTRACT_DIR}/PREPARE_SAVANNA_ACTIONS.json
-printf "," >> ${CONTRACT_DIR}/PREPARE_SAVANNA_ACTIONS.json
+cat ${ACTIONS_DIR}/setcontract-eosio.system.json | jq '.actions[0]' >> ${ACTIONS_DIR}/PREPARE_SAVANNA_ACTIONS.json
+printf "," >> ${ACTIONS_DIR}/PREPARE_SAVANNA_ACTIONS.json
 # add system contract set abi action
-cat ${CONTRACT_DIR}/setcontract-eosio.system.json | jq '.actions[1]' >> ${CONTRACT_DIR}/PREPARE_SAVANNA_ACTIONS.json
+cat ${ACTIONS_DIR}/setcontract-eosio.system.json | jq '.actions[1]' >> ${ACTIONS_DIR}/PREPARE_SAVANNA_ACTIONS.json
 # close actions array
-echo '],' >> ${CONTRACT_DIR}/PREPARE_SAVANNA_ACTIONS.json
+echo '],' >> ${ACTIONS_DIR}/PREPARE_SAVANNA_ACTIONS.json
 # close our transaction
-cat ${CONTRACT_DIR}/end-shell-transaction.json >> ${CONTRACT_DIR}/PREPARE_SAVANNA_ACTIONS.json
-cat ${CONTRACT_DIR}/PREPARE_SAVANNA_ACTIONS.json | jq > /tmp/pretty.json
-mv /tmp/pretty.json ${CONTRACT_DIR}/PREPARE_SAVANNA_ACTIONS.json
+cat ${ACTIONS_DIR}/end-shell-transaction.json >> ${ACTIONS_DIR}/PREPARE_SAVANNA_ACTIONS.json
+cat ${ACTIONS_DIR}/PREPARE_SAVANNA_ACTIONS.json | jq > /tmp/pretty.json
+mv /tmp/pretty.json ${ACTIONS_DIR}/PREPARE_SAVANNA_ACTIONS.json
 
 ###############
 ## SECOND TRX
 ## SWTICHTOSVNN
 ###############
-cleos --url $JUNGLE push action eosio switchtosvnn '{}' -s -d --json-file ${CONTRACT_DIR}/switchtosvnn.json --expiration 8640000
+cleos --url $JUNGLE push action eosio switchtosvnn '{}' -s -d --json-file ${ACTIONS_DIR}/switchtosvnn.json --expiration 8640000
 # using our shell create new transaction with many inline actions
-cp  ${CONTRACT_DIR}/start-shell-transaction.json ${CONTRACT_DIR}/SWITCH_TO_SVNN.json
+cp  ${ACTIONS_DIR}/start-shell-transaction.json ${ACTIONS_DIR}/SWITCH_TO_SVNN.json
 # open actions array
-printf '"actions": [' >> ${CONTRACT_DIR}/SWITCH_TO_SVNN.json
+printf '"actions": [' >> ${ACTIONS_DIR}/SWITCH_TO_SVNN.json
 # append actions with comma sep
-cat ${CONTRACT_DIR}/switchtosvnn.json | jq '.actions[]' >> ${CONTRACT_DIR}/SWITCH_TO_SVNN.json
-rm ${CONTRACT_DIR}/switchtosvnn.json
+cat ${ACTIONS_DIR}/switchtosvnn.json | jq '.actions[]' >> ${ACTIONS_DIR}/SWITCH_TO_SVNN.json
+rm ${ACTIONS_DIR}/switchtosvnn.json
 
 # close actions array
-echo '],' >> ${CONTRACT_DIR}/SWITCH_TO_SVNN.json
+echo '],' >> ${ACTIONS_DIR}/SWITCH_TO_SVNN.json
 # close our transaction
-cat ${CONTRACT_DIR}/end-shell-transaction.json >> ${CONTRACT_DIR}/SWITCH_TO_SVNN.json
-cat ${CONTRACT_DIR}/SWITCH_TO_SVNN.json | jq > /tmp/pretty.json
-mv /tmp/pretty.json ${CONTRACT_DIR}/SWITCH_TO_SVNN.json
+cat ${ACTIONS_DIR}/end-shell-transaction.json >> ${ACTIONS_DIR}/SWITCH_TO_SVNN.json
+cat ${ACTIONS_DIR}/SWITCH_TO_SVNN.json | jq > /tmp/pretty.json
+mv /tmp/pretty.json ${ACTIONS_DIR}/SWITCH_TO_SVNN.json
 
 # clean up files we don't need
-rm ${CONTRACT_DIR}/start-shell-transaction.json ${CONTRACT_DIR}/end-shell-transaction.json ${CONTRACT_DIR}/setcontract-eosio.system.json
+rm ${ACTIONS_DIR}/start-shell-transaction.json ${ACTIONS_DIR}/end-shell-transaction.json ${ACTIONS_DIR}/setcontract-eosio.system.json
--- send_multi_sig.sh
+++ send_multi_sig.sh
@@ -1,7 +1,12 @@
 #!/bin/env bash
 
 NETWORK=${1:-LOCAL}
-CONTRACT_DIR="/local/eosnetworkfoundation/repos/eos-system-contracts/build/contracts"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ACTIONS_DIR="${SCRIPT_DIR}/actions"
+if [ ! -d $ACTIONS_DIR ]; then
+  echo "${ACTIONS_DIR} does not exist"
+  exit
+fi
 
 #PUBLIC_SIG_KEY=EOS81nrWtjvMfDi9E7ddb5nbub2hBWWg6Kih7Y5oTuNPFv5mE72zN
 
@@ -33,28 +38,28 @@ if [ ! -s .eosc-vault-${ACCOUNT}.json ]; then
   eosc vault create --vault-file .eosc-vault-${ACCOUNT}.json --import
 fi
 
-if [ ! -s ${CONTRACT_DIR}/PREPARE_SAVANNA_ACTIONS.json ]; then
-  echo "ERROR: failed to find ${CONTRACT_DIR}/PREPARE_SAVANNA_ACTIONS.json EXITING"
+if [ ! -s ${ACTIONS_DIR}/PREPARE_SAVANNA_ACTIONS.json ]; then
+  echo "ERROR: failed to find ${ACTIONS_DIR}/PREPARE_SAVANNA_ACTIONS.json EXITING"
   exit
 fi
-cat ${CONTRACT_DIR}/PREPARE_SAVANNA_ACTIONS.json | jq 1> /dev/null
+cat ${ACTIONS_DIR}/PREPARE_SAVANNA_ACTIONS.json | jq 1> /dev/null
 if [ $? != 0 ]; then
   echo "ERROR: invalid JSON for PREPARE_SAVANNA_ACTIONS.json EXITING"
   exit
 fi
 eosc -u $ENDPOINT multisig propose $ACCOUNT spring.upd \
-    ${CONTRACT_DIR}/PREPARE_SAVANNA_ACTIONS.json \
+    ${ACTIONS_DIR}/PREPARE_SAVANNA_ACTIONS.json \
     --request-producers --vault-file .eosc-vault-${ACCOUNT}.json
 
-if [ ! -s ${CONTRACT_DIR}/PREPARE_SAVANNA_ACTIONS.json ]; then
-  echo "ERROR: failed to find ${CONTRACT_DIR}/SWITCH_TO_SVNN.json EXITING"
+if [ ! -s ${ACTIONS_DIR}/PREPARE_SAVANNA_ACTIONS.json ]; then
+  echo "ERROR: failed to find ${ACTIONS_DIR}/SWITCH_TO_SVNN.json EXITING"
   exit
 fi
-cat ${CONTRACT_DIR}/SWITCH_TO_SVNN.json | jq 1> /dev/null
+cat ${ACTIONS_DIR}/SWITCH_TO_SVNN.json | jq 1> /dev/null
 if [ $? != 0 ]; then
   echo "ERROR: invalid JSON for SWITCH_TO_SVNN.json EXITING"
   exit
 fi
 eosc -u $ENDPOINT multisig propose $ACCOUNT spring.svn \
-    ${CONTRACT_DIR}/SWITCH_TO_SVNN.json \
+    ${ACTIONS_DIR}/SWITCH_TO_SVNN.json \
     --request-producers --vault-file .eosc-vault-${ACCOUNT}.json
